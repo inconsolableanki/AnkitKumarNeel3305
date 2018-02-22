bb7ba13d6e063e61dc930ae6e62749f82a8f8ac1
ANKIT KUMAR <ankitkumar3305@gmail.com>
2018-02-22 16:35:43 +0530

    post/put/get/intro

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("helloworld");
+const Express = require('express')
+
+var app = Express();
+
+app.listen(3000,()=>{
+
+    console.log("server is running") // these are all use cases. express must follow the order
+}) 
+
+app.get('/',(req,res,next)=>{
+
+    res.send("ankit is running express")
+})
+app.put('/',(req,res,next)=>{
+
+    res.send("put is wroking perfectly")
+})
+
+app.post('/',(req,res,next)=>{
+
+    res.send("post is working perfectly")
+
+})
+
+app.delete('/',(req,res,next)=>{
+    res.send("delete is working perfectly")
+})
