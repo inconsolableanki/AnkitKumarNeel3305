35149a190d5c139a48ee8dad5793a7a59218c4b0
ANKIT KUMAR <ankitkumar3305@gmail.com>
2018-02-22 18:52:16 +0530

    hwusemiddle/gettokenfromserver/davunveristy

--- app.js
+++ app.js
@@ -2,15 +2,30 @@ const Express = require('express')
 
 var app = Express();
 
-app.listen(3000,()=>{
+let middleware=(req,res,next)=>{
 
-    console.log("server is running") // these are all use cases. express must follow the order
-}) 
+    console.log("Middleware is running Successfully ")
+    //console.log(req)
+    let auth = req.headers['authorization'] // when u open arc then u check hearder option there is type autherization
+    console.log("auth is ",auth) // this line with print the token 
+    next();
+}
+app.use('/admin',middleware)
+
+
+app.get('/user/id',(req,res,next)=>{
+
+    console.log("user id is call")
+    res.send("this is user id section")
+})
 
 app.get('/',(req,res,next)=>{
 
     res.send("ankit is running express")
 })
+
+
+
 app.put('/',(req,res,next)=>{
 
     res.send("put is wroking perfectly")
@@ -25,3 +40,8 @@ app.post('/',(req,res,next)=>{
 app.delete('/',(req,res,next)=>{
     res.send("delete is working perfectly")
 })
+
+app.listen(3000,()=>{
+
+    console.log("server is running") // these are all use cases. express must follow the order
+}) 
