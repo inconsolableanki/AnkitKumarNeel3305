61ac48cc8c90fcb9e5143a08ccad5f8aeeda9367
ANKIT KUMAR <ankitkumar3305@gmail.com>
2018-03-01 03:16:26 +0530

    config/done

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const Express = require('express')
 const bodyParser = require('body-parser')
 const routerIndex = require('./routes/index')
+const config = require('config')
+
+let hostConfig = config.get('appConfig.hostConfig')
 
 
-
+const PORT = hostConfig.port || 3600;
 
 const app = Express();
 
@@ -15,7 +18,7 @@ app.get("/",(req,res,next)=>{
     res.send("HELLO WORLD")
 })
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
 
-    console.log("server is running") // these are all use cases. express must follow the order
+    console.log("server is running"+ PORT) // these are all use cases. express must follow the order
 }) 
